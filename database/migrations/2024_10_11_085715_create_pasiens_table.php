2210b1a9c6d080b7f29c64e7c42a4af8b3493233
randykapratama <randykapratama722@gmail.com>
2024-10-31 19:03:30 +0700

    Deskripsi perubahan

---
+++ database/migrations/2024_10_11_085715_create_pasiens_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePasiensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pasiens', function (Blueprint $table) {
+            $table->id();
+            $table->string('no_pasien')->unique(); // Kolom no_pasien ditambahkan
+            $table->string('nama');
+            $table->integer('umur');
+            $table->enum('jenis_kelamin', ['laki-laki', 'perempuan']);
+            $table->string('alamat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pasiens');
+    }
+}
